74ccff1394249ff72475de37425021475bff4af7
chenweibin <chenweibin@chenweibindeMacBook-Pro.local>
2017-09-23 23:59:44 +0800

    12

--- MyTest/ShowView.h
+++ MyTest/ShowView.h
@@ -1,5 +1,5 @@
 //
-//  MainViewController.h
+//  ShowView.h
 //  MyTest
 //
 //  Created by chenweibin on 2017/9/23.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MainViewController : UIViewController
+@interface ShowView : UIView
 
 @end
---
+++ MyTest/ShowView.m
@@ -0,0 +1,21 @@
+//
+//  ShowView.m
+//  MyTest
+//
+//  Created by chenweibin on 2017/9/23.
+//  Copyright © 2017年 chenweibin. All rights reserved.
+//
+
+#import "ShowView.h"
+
+@implementation ShowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
